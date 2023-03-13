76fc4198a4545ba89e2ab355281833f0f68706b7
miroshal <125488565+miroshal@users.noreply.github.com>
2023-03-14 00:40:07 +0300

    Задача 66

---
+++ dz66/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int Prompt(string massege)
+{
+    System.Console.Write(massege);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int GetSum(int min, int max)
+{
+    if (min == max) return max;
+    else return GetSum(min + 1, max) + min;
+}
+
+int n = Prompt("Введи первое число: ");
+int m = Prompt("Введи последнее число: ");
+
+System.Console.WriteLine(GetSum(n, m));
